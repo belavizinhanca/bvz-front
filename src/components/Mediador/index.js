d287b13c984ddb55b664c550032316fa5b18f3c5
Guilherme Alvarez <gui06barboza@gmail.com>
2021-02-18 17:26:39 -0300

    pagina quem somos responsiva

--- src/components/Mediador/index.js
+++ src/components/Mediador/index.js
@@ -7,18 +7,18 @@ import './Mediador.css'
 function Mediador() {
     return(
         <section className="section-mediador">
-            <div className="container-text">
+            <div className="container-flex">
                 <Container>
                     <h2>O que é um Mediador?</h2>
                     <p>Os mediadores são aqueles que irão oferecer ajuda as pessoas que moram na rua, acompanhando a situação delas e comunicando as suas necessidades.</p>
                 </Container>
             </div>
 
-            <div className="container-text seja-mediador">
+            <div className="container-flex sejamediador">
                 <Container>
                     <h2>Como se tornar um Mediador?</h2>
                     <p>Tornar-se um Mediador é bem simples basta você clicar no botão ao lado para fazer o cadastro e depois disso você já estará apto a começar a fazer o bem!</p>
-                    <a className="button-mediador" as={Link} href="/cadastro-mediador" to="/cadastro-moderador">Seja Mediador</a>
+                    <a className="button" href="/seja-mediador" to="/seja-mediador">Seja Mediador!</a>
                 </Container>
             </div>
         </section>
