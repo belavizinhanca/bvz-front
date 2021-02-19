885f805256d3e3d55b27e5c2667aee72c174b5dd
Guilherme Alvarez <gui06barboza@gmail.com>
2021-02-19 15:47:27 -0300

    texto da home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -9,8 +9,10 @@ function Home() {
         <section className="section-home">
             <Container className="container-home">
                 <div className="container-flex text">
-                    <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam lobortis massa lorem, sit amet sollicitudin lorem vulputate non. Nulla facilisi.</p>
+                    <h2>Projeto Bela Vizinhança, faça o bem sem olhar a quem.</h2>
+                    <p>A empatia é certamente um dos mais nobres sentimentos humanos. Para entender e ajudar o próximo e necessário se imaginar na condição dele
+Acreditamos que existe sempre uma saída, até mesmo para os problemas quem parecem impossível de superar
+Que o desejo de ajudar o próximo consiga sempre superar a falta de esperança no ser humano.</p>
                     <a href="/quem-somos" to="/quem-somos">Saiba Mais</a>
                 </div>
 
