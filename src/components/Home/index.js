4767e4e887b08429ae6f5403104119297f26fad0
Guilherme Alvarez <gui06barboza@gmail.com>
2021-02-19 20:25:44 -0300

    imagens na home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,21 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Container, Carousel } from 'react-bootstrap'
+import { useState } from 'react'
+
 import home from '../images/home.png'
+import image1 from '../images/image1.jpeg'
+import image2 from '../images/image2.jpeg'
+import image3 from '../images/image3.jpeg'
 
 import './Home.css'
 
 function Home() {
+    const [index, setIndex] = useState(0);
+
+    const handleSelect = (selectedIndex, e) => {
+        setIndex(selectedIndex);
+    };
+
     return(
         <section className="section-home">
             <Container className="container-home">
@@ -13,10 +24,32 @@ function Home() {
                     <p>A empatia é certamente um dos mais nobres sentimentos humanos. Para entender e ajudar o próximo e necessário se imaginar na condição dele
     Acreditamos que existe sempre uma saída, até mesmo para os problemas quem parecem impossível de superar
     Que o desejo de ajudar o próximo consiga sempre superar a falta de esperança no ser humano.</p>
-    <a href="/quem-somos" to="/quem-somos">Saiba Mais</a>
+    <a className="button-saibamais" href="/quem-somos" to="/quem-somos">Saiba Mais</a>
                 </div>
                 <div className="container-flex-home">
-                    <img src={home} alt="Imagem home" />
+                <Carousel activeIndex={index} onSelect={handleSelect}>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src={image1}
+                        alt="First slide"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src={image2}
+                        alt="Second slide"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src={image3}
+                        alt="Third slide"
+                        />
+                    </Carousel.Item>
+                </Carousel>
                 </div>
             </Container>
         </section>
