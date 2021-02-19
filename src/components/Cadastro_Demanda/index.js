d15780b6851be0b9e2856f2a655122cc0d1e48ba
Guilherme Alvarez <gui06barboza@gmail.com>
2021-02-18 22:28:16 -0300

    cadastro demanda conectado ao back

--- src/components/Cadastro_Demanda/index.js
+++ src/components/Cadastro_Demanda/index.js
@@ -1,27 +1,57 @@
 import React from 'react'
 import { Container, Form, Button} from 'react-bootstrap'
+import { useState } from 'react'
+import { useHistory } from 'react-router-dom'
 
 import './CadDemanda.css'
 
 function CadDemanda() {
+    const [form, setForm] = useState({
+        nome: '',
+        perfil: '',
+        demanda: ''
+    })
+
+    const history = useHistory()
+
+    function handleChange({target}) {
+        const {id, value} = target
+
+        setForm({  ...form, [id]:value })
+        console.log({ [id]:value  })
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        fetch('https://bvz-back.herokuapp.com/moradores', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(form)
+        })
+
+        history.push("/doacao")
+    }
+
     return(
         <section className="form-section">
             <Container className="form-container">
-                <h2>Cadastre-se</h2>
-                <Form>
-                    <Form.Group>
-                        <Form.Label htmlFor="foto">Foto</Form.Label>
-                        <Form.Control type="file" placeholder="Foto" id="foto" />
-                    </Form.Group>
-
+                <h2>Cadastrar demanda</h2>
+                <Form onSubmit={handleSubmit}>
                     <Form.Group>
                         <Form.Label htmlFor="nome">Nome</Form.Label>
-                        <Form.Control type="text" placeholder="Nome" id="nome" />
+                        <Form.Control type="text" value={form.nome} placeholder="Nome" id="nome" onChange={handleChange}/>
                     </Form.Group>
 
                     <Form.Group>
-                        <Form.Label htmlFor="demandas">Demandas</Form.Label>
-                        <Form.Control as="textarea" rows={4} placeholder="Demandas" id="demandas" />
+                        <Form.Label htmlFor="perfil">Perfil</Form.Label>
+                        <Form.Control type="text" value={form.perfil} placeholder="Conte a história dessa pessoa" id="perfil" onChange={handleChange}/>
+                    </Form.Group>
+
+                    <Form.Group>
+                        <Form.Label htmlFor="demanda">Demandas</Form.Label>
+                        <Form.Control as="textarea" value={form.demanda} rows={4} placeholder="Diga o que essa pessoa precisa" id="demanda" onChange={handleChange}/>
                     </Form.Group>
 
                     <Button className="button-enviar" type="submit">
