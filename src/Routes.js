bb80d436f57e03dd4733b5878bd74a956a56f93c
Roney C. Lima <69720380+RoneyLima@users.noreply.github.com>
2021-02-27 14:07:23 -0300

    limpeza de arquivos

--- src/Routes.js
+++ src/Routes.js
@@ -1,30 +1,30 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
-import Home from './components/Home'
-import QuemSomos from './components/Quem_Somos'
-import Mediador from './components/Mediador'
-import CadastroMediador from './components/Cadastro_Mediador'
-import Demandas from './components/Demandas'
-import CadDemanda from './components/Cadastro_Demanda'
-import Contato from './components/Contato'
-import Gerenciar from './components/Gerenciar'
-import Login from './components/Login'
-import Cadastro from './components/Cadastro'
+import Home                 from './components/Home'
+import QuemSomos            from './components/Quem_Somos'
+import Mediador             from './components/Mediador'
+import CadastroMediador     from './components/Cadastro_Mediador'
+import Demandas             from './components/Demandas'
+import CadDemanda           from './components/Cadastro_Demanda'
+import Contato              from './components/Contato'
+import Gerenciar            from './components/Gerenciar'
+import Login                from './components/Login'
+import Cadastro             from './components/Cadastro'
 
 function Routes() {
     return(
         <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/quem-somos" component={QuemSomos} />
-            <Route exact path="/contato" component={Contato} />
-            <Route exact path="/mediador" component={Mediador} />
-            <Route exact path="/cadastro-mediador" component={CadastroMediador} />
-            <Route exact path="/doacao" component={Demandas} />
-            <Route exact path="/cadastrar-demanda" component={CadDemanda} />
-            <Route exact path="/entrar" component={Login} />
-            <Route exact path="/cadastro" component={Cadastro} />
-            <Route exact path="/gerenciar" component={Gerenciar} />
+            <Route exact path="/"                       component={Home} />
+            <Route exact path="/quem-somos"             component={QuemSomos} />
+            <Route exact path="/contato"                component={Contato} />
+            <Route exact path="/mediador"               component={Mediador} />
+            <Route exact path="/cadastro-mediador"      component={CadastroMediador} />
+            <Route exact path="/doacao"                 component={Demandas} />
+            <Route exact path="/cadastrar-demanda"      component={CadDemanda} />
+            <Route exact path="/entrar"                 component={Login} />
+            <Route exact path="/cadastro"               component={Cadastro} />
+            <Route exact path="/gerenciar"              component={Gerenciar} />
         </Switch>
     )
 }
