1eab07026e5cc1b78dcce45e1739203ef43ba21b
Roney C. Lima <69720380+RoneyLima@users.noreply.github.com>
2021-03-01 14:54:32 -0300

    mudança no background - não finalizado

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 import { withRouter } from 'react-router-dom'
 import { Navbar, Nav, NavDropdown, Container, Image } from 'react-bootstrap'
 import logo from '../images/logobvznovoLinha.png'
@@ -6,22 +9,36 @@ import logo from '../images/logobvznovoLinha.png'
 import './Menu.css'
 
 function BaseMenu() {
+
+    const [anchorEl, setAnchorEl] = React.useState('');
+
+    const handleClick = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+  
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
+
+    
+
     return(
         <Navbar fixed="top" variant="dark" expand="lg">
         <Container>
 
             <Navbar.Brand href="/" to="/"><Image src={logo} alt="Logo Bela Vizinhança" width="320" /></Navbar.Brand>
-
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto">
                 <Nav.Link href="/" to="/">Home</Nav.Link>
                 <Nav.Link href="/quem-somos" to="/quem-somos">Quem Somos</Nav.Link>
-                <NavDropdown title="Como Ajudar?" id="basic-nav-dropdown">
+    
+                <NavDropdown anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} title="Como Ajudar?" id="basic-nav-dropdown" onMouseOut={handleClose} onMouseOver={handleClick}>
                     <NavDropdown.Item href="/mediador" to="/mediador">Mediador</NavDropdown.Item>
                     <NavDropdown.Divider />
                     <NavDropdown.Item href="/doacao" to="/doacao">Doação</NavDropdown.Item>
                 </NavDropdown>
+                
                 <Nav.Link href="/contato" to="/contato">Contato</Nav.Link>
                 <Nav.Link href="/gerenciar" to="/gerenciar">Gerenciar</Nav.Link>
                 <Nav.Link href="/app" to="/app">App</Nav.Link>
@@ -35,6 +52,6 @@ function BaseMenu() {
     )
 }
 
-const Menu = withRouter(BaseMenu)
+const Menuex = withRouter(BaseMenu)
 
-export default Menu;
+export default Menuex;
