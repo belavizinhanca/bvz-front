bd156aed6f6a8470046e75ae88f546b898eab355
Guilherme Alvarez <gui06barboza@gmail.com>
2021-03-02 17:53:58 -0300

    mudando pra home

--- src/rotas/Home/index.js
+++ src/rotas/Home/index.js
@@ -16,35 +16,54 @@ function Home() {
     };
 
     return(
+        <>
         <section className="section-home">
             <Container className="container-home">
                 <div className="container-flex-home text">
                     <h2>Projeto Bela Vizinhança, faça o bem sem olhar a quem.</h2>
                     <p>A empatia é certamente um dos mais nobres sentimentos humanos. Para entender e ajudar o próximo e necessário se imaginar na condição dele
-    Acreditamos que existe sempre uma saída, até mesmo para os problemas quem parecem impossível de superar
-    Que o desejo de ajudar o próximo consiga sempre superar a falta de esperança no ser humano.</p>
-    <a className="button-saibamais" href="/quem-somos" to="/quem-somos">Saiba Mais</a>
-                </div>
-                <div className="container-flex-home">
-                <Carousel activeIndex={index} onSelect={handleSelect}>
-                    <Carousel.Item>
-                        <img
-                        className="d-block w-100"
-                        src={image1}
-                        alt="First slide"
-                        />
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img
-                        className="d-block w-100"
-                        src={image3}
-                        alt="Third slide"
-                        />
-                    </Carousel.Item>
-                </Carousel>
+                    Acreditamos que existe sempre uma saída, até mesmo para os problemas quem parecem impossível de superar
+                    Que o desejo de ajudar o próximo consiga sempre superar a falta de esperança no ser humano.</p>
+                    <a className="button-saibamais" href="/quem-somos" to="/quem-somos">Saiba Mais</a>
                 </div>
             </Container>
         </section>
+
+        <section className="section-projetos">
+            <Container className="container-projetos">
+                <div className="title-projetos">
+                    <h2>Conheça Os Projetos Da Região</h2>
+                </div>
+
+                <div className="container-flex">
+                    <div className="text-container">
+                        <h2>Nome do projeto</h2>
+                        <p>Conteudo do projeto...</p>
+                    </div>
+
+                    <div className="img-container">
+                        <div className="img-provisorio">
+                            
+                        </div>
+                    </div>
+                </div>
+
+                <div className="container-flex" style={{flexDirection: 'row-reverse'}}>
+                    <div className="text-container">
+                        <h2>Nome do projeto</h2>
+                        <p>Conteudo do projeto...</p>
+                    </div>
+
+                    <div className="img-container">
+                        <div className="img-provisorio">
+
+                        </div>
+                    </div>
+                </div>
+            </Container>
+        </section>
+
+        </>
     )
 }
 
